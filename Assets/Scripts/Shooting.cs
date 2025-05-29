67683df3aa6f0215fc7bf62264e3fd426349a3b1
PrismFae <sky0522.c@icloud.com>
2025-05-29 09:39:08 -0400

    First Playable

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -9,14 +9,10 @@ public class Shooting : MonoBehaviour
     public float timeUntilReloaded = 0;
     public float fireRate = 1; // shots per secend 
     public int shootMode = 1;
+    public float detectionRange = 10f; // Range within which the player can shoot
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Transform targetEnemy;
 
-    // Update is called once per frame
     void Update()
     {
         isTriggerDown = Input.GetButtonDown("Jump");
@@ -25,12 +21,64 @@ public class Shooting : MonoBehaviour
             shootMode = 1;
 
         }
-     
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            shootMode = 2;
+        }
+
         if (shootMode == 1)
         {
             BasicShootingBehaviour();
         }
-    
+        else if (shootMode == 2)
+        {
+            AutoAimandShoot();
+        }
+
+    }
+
+    void AutoAimandShoot()
+    {
+        FindNearestEnemy();
+
+        if (targetEnemy != null)
+        {   
+            // Calculate direction in 2D
+            Vector2 direction = (targetEnemy.position - transform.position).normalized;
+            // Calulate angle and rotate only around Z axis
+           float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+
+            //Shoot if reloaded
+            if (timeUntilReloaded <= 0)
+            {
+                Instantiate(projectilePrefab, transform.position, transform.rotation);
+                float secondsPerShot = 1 / fireRate;
+                timeUntilReloaded += secondsPerShot;
+            }
+        }
+
+        timeUntilReloaded -= Time.deltaTime;
+        if (timeUntilReloaded <= 0)
+        {
+            timeUntilReloaded = 0;
+        }
+    }
+    void FindNearestEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        float closestDistance = detectionRange;
+        targetEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                targetEnemy = enemy.transform;
+            }
+        }
     }
 
 
@@ -55,4 +103,3 @@ public class Shooting : MonoBehaviour
 
  
 }
-
